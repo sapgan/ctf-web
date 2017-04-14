c8fa3ce1eedf36011efd17dd0a7e9ced6ae447d9
Saptarshi Gan <sapgan@iitk.ac.in>
2017-04-15 02:41:01 +0530

    Run all challenges from diff vms

--- scripts/setup.py
+++ scripts/setup.py
@@ -1,10 +1,10 @@
 import sqlite3
 
-conn = sqlite3.connect('../users.db')
+conn = sqlite3.connect('/home/vagrant/users.db')
 c = conn.cursor()
 c.execute('CREATE TABLE users (username text, password text);')
 
-ufp = "shell_details"
+ufp = "/home/vagrant/scripts/shell_details"
 uf = open(ufp,"r").readlines()
 for user in uf:
     u,p = user.strip().split(":")
