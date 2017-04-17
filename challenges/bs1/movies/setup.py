12d0d3a1be3e5671dee5ecf80b919b8665c4fa45
Saptarshi Gan <sapgan@iitk.ac.in>
2017-04-18 03:53:18 +0530

    Added blind1

---
+++ challenges/bs1/movies/setup.py
@@ -0,0 +1,19 @@
+import sqlite3
+from hashlib import md5
+secret = "asdasd98a9ndand987a83nuindna9721n9n923"
+movies = [{"Dark Knight Rises", "Nolan"},{"Godfather", "Brando"},{"Shawshank Redemption","Freeman"},{"Batman","Nolan"},{"Avengers","Marvel"}]
+ufp = "/home/vagrant/scripts/shell_details"
+uf = open(ufp,"r").readlines()
+for user in uf:
+    u,p = user.strip().split(":")
+    flag = "cs628a{"+md5((secret+u).encode('utf-8')).hexdigest()+"}"
+    movies.append({"",flag})
+    db_name = "movies_"+u+".db"
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
+    c.execute('CREATE TABLE movies (title text, director text);')
+    for title,director in movies:
+        stmt = "INSERT INTO movies VALUES ('{}','{}')".format(title,director)
+        c.execute(stmt)
+    conn.commit()
+    conn.close()
